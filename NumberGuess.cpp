eb3b2ea14a910eaa6f1cefa3590f25ace38724e9
Ijlal Ahmed <141372268+Ijlal-Ahmed10@users.noreply.github.com>
2023-08-19 04:49:01 +0500

    Add files via upload

---
+++ NumberGuess.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int& numCheck(int&);
+
+
+int main()
+{
+    srand(time(NULL));      //to generate random number on each run
+    int randNum = rand() % 101;
+    int num;
+    
+    cout << "\n---------------------*  NUMBER GUESS GAME  *---------------------\n";
+    cout << "\n Instructions:  Guess the number between the range 0-100\n";
+
+   
+    while (true)
+    {
+        
+        
+        cout<<"\n - Enter your guess =  ";
+        num = numCheck(num);
+        
+        if (num > randNum) cout << "\n  \t Too high, guess lower\n";
+        else if (num<randNum) cout <<"\n  \t Too low, try a little higher\n";
+        else {
+            cout<<"\n\n \t\t*****  CORRECT! *****\n     CONGRATULATIONS! YOU GUESSED THE NUMBER! :D\n";
+            break;
+        }
+    }
+    cout << "\n \t\tThe number was  '" << randNum << "'\n\n\n";
+
+    for (int i = 0; i < 66; i++)cout << "-";
+    cout << "\n\n\n";
+
+    return 0;
+}
+
+
+int& numCheck(int& n)
+{
+    cin >> n;
+    while (cin.fail() || n > 100 || n < 0)
+    {
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore();
+            cout << "\n\t ***  Error! Input must be numeric  ***\n";
+            cout << "\n - Enter your guess = ";
+            cin >> n;
+        }
+        else
+        {
+            cout << "\n\t ***  Error! Input must be in the range 0-100  ***\n";
+            cout << "\n - Enter your guess = ";
+            cin >> n;
+        }
+    }
+    return n;
+}
